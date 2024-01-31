88519a31d218863eb32a87308a7cee7585a3dbf2
Duy Dương <dexter@Duys-MacBook-Pro.local>
2024-01-31 14:19:40 +0700

    render fix

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ import { HashRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <HashRouter>
       <App />
     </HashRouter>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
