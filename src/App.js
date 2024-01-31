2404703712458f3cd805c65ec79e75ea8b49e117
Duy Dương <dexter@Duys-MacBook-Pro.local>
2024-01-31 14:26:35 +0700

    hash

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import "./App.css";
 import RotateCore from "./rubik/components/RotateCore";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Desk from "./Desk/Desk";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Desk />} />
         <Route path="rubik" element={<RotateCore />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
